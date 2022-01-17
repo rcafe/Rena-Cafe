16ca2ddb6dd4feb65cd586ec309706c0c9228cd8
Vyan4033 <vyanc403@gmail.com>
2022-01-18 00:07:50 +0700

    commit

---
+++ pages/detail-cafe/[id].js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Link from "next/link";
+import DetailData from "../../components/detailData";
+import Header from "../../components/header";
+
+export async function getServerSideProps(context) {
+    return { props: { id: context.params.id } };
+}
+    const Detail = (props) => {
+        return (
+            <>
+                <Head>
+                    <title>{props.id}</title>
+                </Head>
+                <Header/>
+                <DetailData id={props.id} />
+            </>
+    );
+};
+export default Detail;
+    
